d8f81d7b9494f6d8875bc14c124b22fb8ebec83f
Soklie <meurn.sokly.2823@rupp.edu.kh>
2025-12-23 08:52:42 +0700

    first commit

---
+++ app/src/main/java/com/example/student_enrollment_app/user/StatusScreenFragment.kt
@@ -0,0 +1,60 @@
+package com.example.student_enrollment_app.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.student_enrollment_app.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StatusScreenFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StatusScreenFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var mParam1: String? = null
+    private var mParam2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (getArguments() != null) {
+            mParam1 = getArguments()!!.getString(ARG_PARAM1)
+            mParam2 = getArguments()!!.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_status_screen, container, false)
+    }
+
+    companion object {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment StatusScreenFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        fun newInstance(param1: String?, param2: String?): StatusScreenFragment {
+            val fragment = StatusScreenFragment()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.setArguments(args)
+            return fragment
+        }
+    }
+}
